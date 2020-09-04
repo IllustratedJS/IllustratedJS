fc4cbd80270981efaa75de0aebf12221fe0f4de9
Jessica Wolff <jleahwolff@gmail.com>
2020-09-04 18:39:21 -0500

    Pair programmed landing, and course pages.

--- illustrtedjs/src/components/marketing/Curriculum.jsx
+++ illustrtedjs/src/components/marketing/Curriculum.jsx
@@ -9,7 +9,7 @@ const Curriculum = () => {
       <img className="currBlob" src={CurrBlob} />
       <div className="curriculum-title">
         <h2>Curriculum</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis sapiente fuga laborum repellendus quod tempore sequi nihil aliquid veritatis distinctio consequatur eius, quis illo deleniti, adipisci, quidem ducimus dolore est?</p>
+        <p>Data Structures, Algorithms and Advanced JavaScript</p>
       </div>
       {/* <div className="curriculum1">
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi cum, enim perferendis inventore dolor corrupti rerum illo excepturi eius, voluptas, et harum. Iure quia tenetur commodi quo maxime consequuntur iste?</p>
--- illustrtedjs/src/components/marketing/Intro.jsx
+++ illustrtedjs/src/components/marketing/Intro.jsx
@@ -9,11 +9,9 @@ const Intro = () => {
 					The <span className='introColor'> JavaScript</span> Computer Science
 					Platform
 				</p>
-				<h1 className='introTitle'>Find Experiences</h1>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Condimentum
-					nullam congue dictum nulla egestas sit nulla quisque. Velit, faucibus
-					consequat neque, condimentum proin tincidunt maecenas ac.
+				<h1 className='introTitle'>Reshape your learning</h1>
+				<p >
+				Computer Science for those without a math and engineering degree. Learn skills to transform your applications, improve performance and crush your interview. Watch complex structures transform into life with examples you can relate to. Get started now with illustrative examples, detailed notes, code snippets, video demos and more. 
 				</p>
 				<a href="#curriculum" className="currButton">Curriculum</a>
 			</div>
