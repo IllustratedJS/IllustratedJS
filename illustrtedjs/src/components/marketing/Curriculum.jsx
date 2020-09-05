2aaabcf9a9c8a609dfee4f222e2484dae80d3516
Jessica Wolff <jleahwolff@gmail.com>
2020-09-04 20:30:20 -0500

    Updating currBlob

--- illustrtedjs/src/components/marketing/Curriculum.jsx
+++ illustrtedjs/src/components/marketing/Curriculum.jsx
@@ -9,17 +9,11 @@ const Curriculum = () => {
       <img className="currBlob" src={CurrBlob} />
       <div className="curriculum-title">
         <h2>Curriculum</h2>
+      </div>
+      <div className="curriculum-body">
         <p>Data Structures, Algorithms and Advanced JavaScript</p>
       </div>
-      {/* <div className="curriculum1">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi cum, enim perferendis inventore dolor corrupti rerum illo excepturi eius, voluptas, et harum. Iure quia tenetur commodi quo maxime consequuntur iste?</p>
-      </div>
-      <div className="curriculum2">
-        <p>Borem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi cum, enim perferendis inventore dolor corrupti rerum illo excepturi eius, voluptas, et harum. Iure quia tenetur commodi quo maxime consequuntur iste?</p>
-      </div>
-      <div className="curriculum3">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi cum, enim perferendis inventore dolor corrupti rerum illo excepturi eius, voluptas, et harum. Iure quia tenetur commodi quo maxime consequuntur iste?</p>
-      </div> */}
+      
     </div>
   )
 }
