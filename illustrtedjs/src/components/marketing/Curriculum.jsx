557a542b28d6783918f88478352c58608528cc3b
Jessica Wolff <jleahwolff@gmail.com>
2020-09-05 14:58:10 -0500

    Landing page gloss and curriculum section.

--- illustrtedjs/src/components/marketing/Curriculum.jsx
+++ illustrtedjs/src/components/marketing/Curriculum.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import CurrBlob from "../../assets/curr-blob.svg";
+import Check from "../../assets/check-mark.svg";
 import "./Curriculum.css";
 
 const Curriculum = () => {
@@ -11,7 +12,36 @@ const Curriculum = () => {
         <h2>Curriculum</h2>
       </div>
       <div className="curriculum-body">
-        <p>Data Structures, Algorithms and Advanced JavaScript</p>
+        <div className="curr-topics">
+          <h4>Data Structures</h4>
+        <ul className="checked-curr">
+          <li><img className="check" src={Check}/> &nbsp; Stacks + Queues</li>
+          <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
+          <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
+          <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
+          <li><img className="check" src={Check}/>.</li>
+        </ul>
+          </div>
+          <div className="curr-topics">
+          <h4>Algorithms</h4>
+        <ul className="checked-curr">
+          <li><img className="check" src={Check}/> &nbsp; Stacks + Queues</li>
+          <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
+          <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
+          <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
+          <li><img className="check" src={Check}/>.</li>
+        </ul>
+          </div>
+        <div className="curr-topics">
+          <h4>Advanced Javascript</h4>
+        <ul className="checked-curr">
+          <li><img className="check" src={Check}/> &nbsp; Stacks + Queues</li>
+          <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
+          <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
+          <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
+          <li><img className="check" src={Check}/>.</li>
+        </ul>
+          </div>
       </div>
       
     </div>
