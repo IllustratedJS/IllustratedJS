55e0a9368e1de2c0cb7109b8b05ab9e1cb6a3ebb
Jessica Wolff <jleahwolff@gmail.com>
2020-10-06 04:52:12 -0500

    Landing done.

--- illustrtedjs/src/components/marketing/Curriculum.jsx
+++ illustrtedjs/src/components/marketing/Curriculum.jsx
@@ -6,6 +6,68 @@ import BottomWave from '../../assets/intro-bottom-wave.svg';
 
 
 const Curriculum = () => {
+
+  //Attempt #1 ---intersectionObserver
+  // const SELECTOR = '.ds';
+  // const ANIMATE = '.animate';
+
+  // const animate = element => (
+  //   element.classList.add(document.querySelector('.ds'))
+  // );
+
+  // const isAnimated = element => (
+  //   element.classList.contains(document.querySelector('animate'))
+  // );
+
+  // const intersectionObserver = new IntersectionObserver((entries, observer) => {
+  //   entries.forEach((entry) => {
+
+  //     if (entry.intersectionRatio > 0) {
+  //       animate(entry.target);
+  //     }
+  //     // observer.unobserve(entry.target); 
+  //   });
+  // });
+
+  // // eachLink.forEach((link, index)
+  // const elements = [].filter.call(
+  //   document.querySelectorAll('.ds'),
+  //   element => !isAnimated(element, document.querySelectorAll('.animate'))
+  // );
+  //   console.log(elements);
+
+  //   elements.forEach((element) => intersectionObserver.observe(element));
+
+  // Attempt #2--------------------
+  // function animate(entries) {
+  //   entries.forEach(function (entry) {
+  //     if (entry.isIntersecting) {
+  //       entry.target.classList.add('animated');
+  //     } else {
+  //       entry.target.classList.remove('animated');
+  //     }
+  //   }
+  //   )}
+
+
+  //   function initObserver() {
+  //     var observer = new IntersectionObserver(animate);
+  //     var items = document.querySelectorAll('.check');
+  //     for(var i in items) {
+  //       if(!items.hasOwnProperty(i)) {
+  //         continue;
+  //       }
+  //       observer.observe(items[i]);
+  //     }
+  //   }
+
+  //   if (window.IntersectionObserver) {
+  //     initObserver();
+  //   } else {
+  //     console.log('Intersection Observer not supported')
+  //   };
+
+
   return(
     <div>
 
@@ -21,7 +83,6 @@ const Curriculum = () => {
               <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
               <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
               <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
-              <li><img className="check" src={Check}/>.</li>
             </ul>
           </div>
           <div className="algo">
@@ -31,7 +92,6 @@ const Curriculum = () => {
               <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
               <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
               <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
-              <li><img className="check" src={Check}/>.</li>
             </ul>
           </div>
         <div className="js">
@@ -41,7 +101,6 @@ const Curriculum = () => {
               <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
               <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
               <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
-              <li><img className="check" src={Check}/>.</li>
             </ul>
           
       </div>
