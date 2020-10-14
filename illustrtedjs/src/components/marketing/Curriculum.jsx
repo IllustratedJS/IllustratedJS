b8ad33ffd2cf1bbcad6e2004349991b35bf1a050
Matt-GitHub <matthewjamesbedard@outlook.com>
2020-10-13 20:59:25 -0400

    updated marketing page

--- illustrtedjs/src/components/marketing/Curriculum.jsx
+++ illustrtedjs/src/components/marketing/Curriculum.jsx
@@ -6,46 +6,74 @@ import BottomWave from '../../assets/intro-bottom-wave.svg';
 import Books from '../../assets/books.svg';
 
 const Curriculum = () => {
-
-  return(
+  return (
     <div>
-
-    <div className="curriculum-box">
-      <a name="curriculum"></a>
-      <div className="curriculum-title">
-        <h2>Curriculum</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil earum autem fuga porro ratione, impedit est ea provident maxime culpa officia consequuntur doloremque quibusdam maiores nam totam in nesciunt delectus.</p>
-      </div>
-        <img className="books" src={Books}/>
+      <div className="curriculum-box">
+        <a name="curriculum"></a>
+        <div className="curriculum-title">
+          <h2>Curriculum</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil
+            earum autem fuga porro ratione, impedit est ea provident maxime
+            culpa officia consequuntur doloremque quibusdam maiores nam totam in
+            nesciunt delectus.
+          </p>
+        </div>
+        <img className="books" src={Books} />
+        {/*         
         <div className="ds">
-            <h4>Data Structures</h4>
-            <ul className="checked-curr">
-              <li><img className="check" src={Check}/> &nbsp; Stacks + Queues</li>
-              <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
-              <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
-              <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
-            </ul>
-          </div>
-          <div className="algo">
-            <h4>Algorithms</h4>
-            <ul className="checked-curr">
-              <li className="curriculum-spaced"><img className="check" src={Check}/> &nbsp; Stacks + Queues</li>
-              <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
-              <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
-              <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
-            </ul>
-          </div>
+          <h4>Data Structures</h4>
+          <ul className="checked-curr">
+            <li>
+              <img className="check" src={Check} /> &nbsp; Stacks + Queues
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Big O Notation
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Singly Linked Lists
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Doubly Linked Lists
+            </li>
+          </ul>
+        </div>
+        <div className="algo">
+          <h4>Algorithms</h4>
+          <ul className="checked-curr">
+            <li className="curriculum-spaced">
+              <img className="check" src={Check} /> &nbsp; Stacks + Queues
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Big O Notation
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Singly Linked Lists
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Doubly Linked Lists
+            </li>
+          </ul>
+        </div>
         <div className="js">
-            <h4>Advanced Javascript</h4>
-            <ul className="checked-curr">
-              <li><img className="check" src={Check}/> &nbsp; Stacks + Queues</li>
-              <li><img className="check" src={Check}/> &nbsp; Big O Notation</li>
-              <li><img className="check" src={Check}/> &nbsp; Singly Linked Lists</li>
-              <li><img className="check" src={Check}/> &nbsp; Doubly Linked Lists</li>
-            </ul>
-
+          <h4>Advanced Javascript</h4>
+          <ul className="checked-curr">
+            <li>
+              <img className="check" src={Check} /> &nbsp; Stacks + Queues
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Big O Notation
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Singly Linked Lists
+            </li>
+            <li>
+              <img className="check" src={Check} /> &nbsp; Doubly Linked Lists
+            </li>
+          </ul>
+        </div> */}
+        <img src={BottomWave} />
       </div>
-      <img src={BottomWave} />
     </div>
   );
 };
--- illustrtedjs/src/components/marketing/Intro.jsx
+++ illustrtedjs/src/components/marketing/Intro.jsx
@@ -1,39 +1,42 @@
 import React from 'react';
 import Lottie from 'react-lottie';
-import computer from "../../assets/lotties/Computer.json";
-      // <Computer loop={true} />
+import computer from '../../assets/lotties/Computer.json';
+// <Computer loop={true} />
 
 const Intro = () => {
-	const defaultOptions = {
-		
-		loop: false,
-		autoplay: true,
-		animationData: computer,
-		rendererSettings: {
-			className: "computer"
-			// preserveAspectRatio: "xMidYMid slice"
-		}
-	};
-	return (
-		<div className='intro-box'>
-			<div className='introText'>
-				<p className="pre-header">
-					THE <span className='introColor'> JAVASCRIPT</span> COMPUTER SCIENCE
-					PLATFORM
-				</p>
-				<h1 className='introTitle'>Reshape your learning</h1>
-				<p className="intro-paragraph">
-				Computer Science for those without a math and engineering degree. Learn skills to transform your applications, improve performance and crush your interview. Watch complex structures transform into life with examples you can relate to. Get started now with illustrative examples, detailed notes, code snippets, video demos and more. 
-				</p>
-				<a href="#curriculum" className="intro-button">Curriculum</a>
-			</div>
-			<div className="computer-box">
-				<Lottie
-					options={defaultOptions}
-					/>
-			</div>
-		</div>
-	);
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: computer,
+    rendererSettings: {
+      className: 'computer'
+      // preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+  return (
+    <div className="intro-box">
+      <div className="introText">
+        <p className="pre-header">
+          THE <span className="introColor"> JAVASCRIPT</span> COMPUTER SCIENCE
+          PLATFORM
+        </p>
+        <h1 className="introTitle">Reshape your learning</h1>
+        <p className="intro-paragraph">
+          Computer Science for those without a math and engineering degree.
+          Learn skills to transform your applications, improve performance and
+          crush your interview. Watch complex structures transform into life
+          with examples you can relate to. Get started now with illustrative
+          examples, detailed notes, code snippets, video demos and more.
+        </p>
+        <a href="#curriculum" className="intro-button">
+          Curriculum
+        </a>
+      </div>
+      <div className="computer-box">
+        <Lottie options={defaultOptions} />
+      </div>
+    </div>
+  );
 };
 
 export default Intro;
